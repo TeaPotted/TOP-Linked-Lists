1756cd2c5661d93819ef1b6acf307c1395740aad
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-06-29 23:48:01 +0800

    create a function append(value), that adds a new node containing [ value ] to the end of the list

--- linked-lists.js
+++ linked-lists.js
@@ -1,7 +1,24 @@
 // LinkedList class which will represent the full list
 class LinkedList {
   head = null; // initially set the head to null
-};
+
+  // create a function append(value), that adds a new node containing [ value ] to the end of the list
+  append(value) {
+    // if head is empty, create a new Node using [ value ] then set head to the newly created Node
+    if (this.head === null) this.head = new Node(value);
+    // else, get the last node in the list, then set it's .nextNode value to a new Node using [ value ] parameter
+    else {
+      let current = this.head; // get the head, this will be the current node we start from
+
+      // while current.nextNode is not null, set current to it's next node
+      while (current.nextNode !== null) {
+        current = current.nextNode;
+      }
+      // once current is the last node, set it's .nextNode value by creating a new Node using [ value ]
+      current.nextNode = new Node(value);
+    }
+  }
+}
 
 // Node class, containing a value property and a nextNode property, set both as null by default
 class Node {
