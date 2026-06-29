f7f866709ccaae0130c777fb1b39de79a24ac675
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-06-29 23:19:32 +0800

    create a Node class, containing a value property and a nextNode property

--- linked-lists.js
+++ linked-lists.js
@@ -2,3 +2,11 @@
 class LinkedList {
   head = null; // initially set the head to null
 };
+
+// Node class, containing a value property and a nextNode property, set both as null by default
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
