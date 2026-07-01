ca4676b4156e4aa55906e9e98a904339c247a392
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-07-01 20:52:25 +0800

    create a function 'at(index)' in LinkedList, that should return the value of the node at the given index

--- linked-lists.js
+++ linked-lists.js
@@ -22,36 +22,51 @@ class LinkedList {
   // create a function prepend(value) adds a new node containing [ value ] to the start of the list.
   prepend(value) {
     // set head to a new Node using value and this.#head as it's .nextNode
-    this.#head = new Node(value, this.#head)
+    this.#head = new Node(value, this.#head);
   }
 
   // a function size(), returns the total number of nodes in the list
-  size() { 
+  size() {
     let i = 0;
     let current = this.#head;
     // while current is not null, increment i and assign current to it's next node
     while (current !== null) {
-      i++
-      current = current.nextNode
+      i++;
+      current = current.nextNode;
     }
-    return i
+    return i;
   }
 
   // function head(), should return the value of the first node in the list. If the list is empty, it should return undefined
   head() {
-    return this.#head ? this.#head.value : undefined
+    return this.#head ? this.#head.value : undefined;
   }
 
   // function tail() should return the value of the final node in the list.  If the list is empty, it should return undefined
   tail() {
     let current = this.#head;
     if (current === null) return undefined;
-    
+
     // while current has a .nextNode value, assign current to it's .nextNode value. until current does'nt have a next node
     while (current.nextNode) {
       current = current.nextNode;
     }
     return current;
+  }
+
+  // function at(index) should return the value of the node at the given index. If there’s no node at the given index, it should return undefined
+  at(index) {
+    // if index is bigger than the size of the list or less than zero, return undefined
+    if (index > this.size() - 1 || index < 0) return undefined;
+    else {
+      let current = this.#head;
+      // use a for loop to assign current to the value of it's .nextNode until i === index, then return current
+      for (let i = 0; i <= this.size() - 1; i++) {
+        // we will start counting from zero (like in an array)
+        if (i === index) return current;
+        current = current.nextNode;
+      }
+    }
   }
 }
 
