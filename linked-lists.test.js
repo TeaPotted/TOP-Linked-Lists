b66ffc468c7a81f989c85bbf5c0427cdef62e592
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-07-03 17:18:04 +0800

    setup a test for head() function in LinkedList

--- linked-lists.test.js
+++ linked-lists.test.js
@@ -4,5 +4,12 @@ test("LinkedList.size() returns total number of nodes in the list", () => {
   const l = new LinkedList();
   l.append("apple");
   l.append("banana");
-  expect(l.size()).toBe(2)
+  expect(l.size()).toBe(2);
+});
+
+test("LinkedList.head() returns the value of the first node in the list", () => {
+  const l = new LinkedList();
+  l.append("apple");
+  l.append("banana");
+  expect(l.head()).toBe("apple");
 });
